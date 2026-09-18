8c4d045c2b3a9bb61ed452b3bb058759a5d653df
Daniel Baxter <danielbaxter362@gmail.com>
2026-09-18 04:10:26 +0100

    Add flags

--- main.cpp
+++ main.cpp
@@ -10,10 +10,19 @@ struct file_data {
     std::string name;
 };
 
+struct flags {
+    bool show_hidden = false;
+    bool show_size = false;
+}
+
 std::string trim_path(const std::string& path)
 {
     std::size_t last = path.find_last_of("/\\");
     return path.substr(last+1);
+}
+
+void print_usage() {
+    std::cout << "Usage: " << argv[0] << "[-sh] [DIRECTORY]" << std::endl;
 }
 
 void print_file_data(const file_data& data)
@@ -23,18 +32,35 @@ void print_file_data(const file_data& data)
 
 int main(int argc, char* argv[]) 
 {
-    if (argc > 2) {
-        std::cout << "Usage: " << argv[0] << " [DIRECTORY]" << std::endl;
+    if (argc > 3) {
+        print_usage();
         return 1;    
     }
     
+    flags optn;
     std::string dir;
-    if (argc == 1) {
-        dir = fsys::current_path();
+    bool flags_set = false;
+    bool dir_set = false;
+
+    for (size_t i = 0; i < argc - 1; i++) {
+        if (*argv[i] == '-' && !flags_set) {
+            for (argv[i]; *argv[i]; argv[i]++) {
+                switch (*p) {
+                    case 's': optn.show_size = true;
+                    case 'h': optn.show_hidden = true;
+                }
+            }
+            flags_set = true;
+        } else if (!dir_set) {
+            dir = argv[i];
+            flags_set = true;
+        } else {
+            print_usage();
+            return 1;
+        }
     }
-    else {
-        dir = argv[1];
-    }
+
+    if (!dir_set) { dir = fsys::current_path(); }
 
     std::vector<file_data> files;
     for (const fsys::directory_entry& entry : fsys::directory_iterator(dir)) {
